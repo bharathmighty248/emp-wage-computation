19c5716ad6c582b682d413d62dbaa0e742459431
bharathmighty248 <bharathmighty248@gmail.com>
2021-07-02 13:09:58 +0530

    Added : wagesForMonth

--- empWage.sh
+++ empWage.sh
@@ -1,19 +1,26 @@
 #!/bin/bash -x
 
-empCheck=$((RANDOM%3))
 empRatePerHr=20
+numOfWorkingDays=20
+totalMonthSalary=0
+for ((day=1; day<=$numOfWorkingDays; day++))
+do
+	empCheck=$((RANDOM%3))
 
-case $empCheck in
-	1)
-		empHrs=8
-	;;
-	2)
-		empHrs=4
-	;;
-	*)
-		empHrs=0
-	;;
-esac
+	case $empCheck in
+		1)
+			empHrs=8
+		;;
+		2)
+			empHrs=4
+		;;
+		*)
+			empHrs=0
+		;;
+	esac
 
-salary=$(($empRatePerHr*$empHrs))
-echo $salary
+	salary=$(($empRatePerHr*$empHrs))
+	totalMonthSalary=$(($totalMonthSalary+$salary))
+done
+
+echo $totalMonthSalary
