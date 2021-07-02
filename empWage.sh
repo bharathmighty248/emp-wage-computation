7bf07afabcb1e6b802e4b6f9ac44add60bf04bff
bharathmighty248 <bharathmighty248@gmail.com>
2021-07-02 12:55:04 +0530

    Added: switchCase

--- empWage.sh
+++ empWage.sh
@@ -3,15 +3,17 @@
 empCheck=$((RANDOM%3))
 empRatePerHr=20
 
-if [ $empCheck -eq 1 ]
-then
-	empHrs=8
-elif [ $empCheck -eq 2 ]
-then
-	empHrs=4
-else
-	empHrs=0
-fi
+case $empCheck in
+	1)
+		empHrs=8
+	;;
+	2)
+		empHrs=4
+	;;
+	*)
+		empHrs=0
+	;;
+esac
 
 salary=$(($empRatePerHr*$empHrs))
 echo $salary
